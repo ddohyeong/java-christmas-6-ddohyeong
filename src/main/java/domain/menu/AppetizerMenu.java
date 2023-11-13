246f94a9948ecc1c5725abdc3fa246869a56aa7a
ddohyeong <dohyeong7026@gmail.com>
2023-11-13 16:30:43 +0900

    feat(AppetizerMenu) : 애피타이저 메뉴 기능 구현
    docs : 애피타이저 메뉴 기능 완료 체크

---
+++ src/main/java/domain/menu/AppetizerMenu.java
@@ -0,0 +1,38 @@
+package domain.menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import domain.menu.menuenum.Appetizer;
+
+public class AppetizerMenu implements Menu<Appetizer> {
+	private final Map<Appetizer, Integer> appetizerMenu;
+
+	public AppetizerMenu() {
+		this.appetizerMenu = new HashMap<>() {{
+				put(Appetizer.TAPAS, 0);
+				put(Appetizer.MUSHROOM_SOUP, 0);
+				put(Appetizer.CAESAR_SALAD, 0);
+			}};
+	}
+
+	@Override
+	public Map<Appetizer, Integer> getMenuItems() {
+		return appetizerMenu;
+	}
+
+	@Override
+	public Appetizer fromString(String input) {
+		for (Appetizer value : Appetizer.values()) {
+			if (value.getMenuName().equals(input)) {
+				return value;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void putMenu(Enum<? extends Enum<?>> category, int amount) {
+		appetizerMenu.put((Appetizer)category, appetizerMenu.get(category) + amount);
+	}
+}
