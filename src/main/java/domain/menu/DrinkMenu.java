29d64b070b41309584f53b78b518b0b5b54bf8c1
ddohyeong <dohyeong7026@gmail.com>
2023-11-13 16:32:36 +0900

    feat(DrinkMenu) : 음료 메뉴 기능 구현
    docs : 음료 메뉴 기능 완료 체크

---
+++ src/main/java/domain/menu/DrinkMenu.java
@@ -0,0 +1,38 @@
+package domain.menu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import domain.menu.menuenum.Drink;
+
+public class DrinkMenu implements Menu<Drink> {
+	private final Map<Drink, Integer> drinkMenu;
+
+	public DrinkMenu() {
+		this.drinkMenu = new HashMap<>() {{
+				put(Drink.ZERO_COKE, 0);
+				put(Drink.RED_WINE, 0);
+				put(Drink.CHAMPAGNE, 0);
+			}};
+	}
+
+	@Override
+	public Map<Drink, Integer> getMenuItems() {
+		return drinkMenu;
+	}
+
+	@Override
+	public Drink fromString(String input) {
+		for (Drink value : Drink.values()) {
+			if (value.getMenuName().equals(input)) {
+				return value;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void putMenu(Enum<? extends Enum<?>> category, int amount) {
+		drinkMenu.put((Drink)category, drinkMenu.get(category) + amount);
+	}
+}
