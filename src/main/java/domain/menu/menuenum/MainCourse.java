b95fd1576da450aab476f802b0407aa2389e3a5d
ddohyeong <dohyeong7026@gmail.com>
2023-11-13 15:42:01 +0900

    feat(MainCourse) : 메인코스 메뉴를 저장한 enum 구현
    docs : 메인코스 기능 완료 체크

---
+++ src/main/java/domain/menu/menuenum/MainCourse.java
@@ -0,0 +1,20 @@
+package domain.menu.menuenum;
+
+public enum MainCourse {
+	T_BONE_STEAK("티본스테이크", 55000),
+	BBQ_RIBS("바비큐립", 54000),
+	SEAFOOD_PASTA("해산물파스타", 35000),
+	CHRISTMAS_PASTA("크리스마스파스타", 25000);
+
+	private final String menuName;
+	private final int price;
+
+	MainCourse(String menuName, int price) {
+		this.menuName = menuName;
+		this.price = price;
+	}
+
+	public String getMenuName() {
+		return this.menuName;
+	}
+}
