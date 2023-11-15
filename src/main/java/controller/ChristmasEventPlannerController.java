79915ef63459e5842515d6050ffd79e7adf2a9f0
ddohyeong <dohyeong7026@gmail.com>
2023-11-15 12:27:31 +0900

    feat(ChristmasEventPlannerController) : 예상 주문 메뉴 검증 및 입력 기능 구현

--- src/main/java/controller/ChristmasEventPlannerController.java
+++ src/main/java/controller/ChristmasEventPlannerController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import domain.MenuManager;
 import domain.ReservationDate;
 import service.ChristmasEventPlannerService;
 import view.OutputView;
@@ -8,13 +9,19 @@ public class ChristmasEventPlannerController {
 	private final OutputView outputView = new OutputView();
 	private final ChristmasEventPlannerService christmasEventPlannerService = new ChristmasEventPlannerService();
 	private ReservationDate reservationDate;
+	private MenuManager menuManager;
 
 	public void run() {
 		outputView.printStartInfo();
 		getExceptedReservationDate();
+		getExceptedOrderMenus();
 	}
 
 	private void getExceptedReservationDate() {
 		reservationDate = christmasEventPlannerService.setReservationDate();
+	}
+
+	private void getExceptedOrderMenus() {
+		menuManager = christmasEventPlannerService.setOrderMenuManager();
 	}
 }
--- src/main/java/service/ChristmasEventPlannerService.java
+++ src/main/java/service/ChristmasEventPlannerService.java
@@ -1,14 +1,30 @@
 package service;
 
+import domain.MenuManager;
+import domain.OrderMenus;
 import domain.ReservationDate;
 import view.InputView;
 
 public class ChristmasEventPlannerService {
 	private final InputView inputView = new InputView();
+	private final MenuManager initManager = new MenuManager();
+
 	public ReservationDate setReservationDate() {
 		while (true) {
 			try {
 				return new ReservationDate(inputView.enterReservationDate());
+			} catch (IllegalArgumentException exception) {
+				System.out.println(exception.getMessage());
+			}
+		}
+	}
+
+	public MenuManager setOrderMenuManager() {
+		while (true) {
+			try {
+				MenuManager orderManager = new MenuManager();
+				orderManager.order(new OrderMenus(initManager, inputView.enterOrderMenus()));
+				return orderManager;
 			} catch (IllegalArgumentException exception) {
 				System.out.println(exception.getMessage());
 			}
