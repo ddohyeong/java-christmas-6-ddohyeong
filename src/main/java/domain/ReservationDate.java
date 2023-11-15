8c49dfeb3b35e9b62ee722327b6faf0a1a6e16d5
ddohyeong <dohyeong7026@gmail.com>
2023-11-15 10:43:27 +0900

    refactor(ReservationDate) : 에러 메시지 수정

--- src/main/java/domain/ReservationDate.java
+++ src/main/java/domain/ReservationDate.java
@@ -23,7 +23,7 @@ public class ReservationDate {
 		Matcher matcher = pattern.matcher(date);
 
 		if (!matcher.matches()) {
-			throw new IllegalArgumentException("[ERROR] 날짜는 숫자만 입력 하세요");
+			throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 		}
 	}
 
