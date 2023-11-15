c88ed0e3de0781e184532640d4b0692fa3c29f4d
ddohyeong <dohyeong7026@gmail.com>
2023-11-15 12:31:14 +0900

    feat(ChristmasEventPlannerController) : 예상 주문 기능 구현

--- src/main/java/controller/ChristmasEventPlannerController.java
+++ src/main/java/controller/ChristmasEventPlannerController.java
@@ -1,6 +1,10 @@
 package controller;
 
+import domain.BenefitCalculator;
+import domain.DecemberEventPlanner;
+import domain.EventBadge;
 import domain.MenuManager;
+import domain.OrderCalculator;
 import domain.ReservationDate;
 import service.ChristmasEventPlannerService;
 import view.OutputView;
@@ -10,11 +14,16 @@ public class ChristmasEventPlannerController {
 	private final ChristmasEventPlannerService christmasEventPlannerService = new ChristmasEventPlannerService();
 	private ReservationDate reservationDate;
 	private MenuManager menuManager;
+	private OrderCalculator orderCalculator;
+	private DecemberEventPlanner decemberEventPlanner;
+	private BenefitCalculator benefitCalculator;
+	private EventBadge eventBadge;
 
 	public void run() {
 		outputView.printStartInfo();
 		getExceptedReservationDate();
 		getExceptedOrderMenus();
+		exceptedOrder();
 	}
 
 	private void getExceptedReservationDate() {
@@ -23,5 +32,12 @@ public class ChristmasEventPlannerController {
 
 	private void getExceptedOrderMenus() {
 		menuManager = christmasEventPlannerService.setOrderMenuManager();
+	}
+
+	private void exceptedOrder() {
+		orderCalculator = new OrderCalculator(menuManager);
+		decemberEventPlanner = new DecemberEventPlanner(reservationDate);
+		benefitCalculator = new BenefitCalculator(decemberEventPlanner, orderCalculator);
+		eventBadge = new EventBadge(benefitCalculator.getTotalBenefitPrice());
 	}
 }
