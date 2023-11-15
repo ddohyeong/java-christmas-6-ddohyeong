7a7c0584a6ac7e263477d8be7ed825cdd30f332f
ddohyeong <dohyeong7026@gmail.com>
2023-11-15 16:04:40 +0900

    feat(InputMessage) : 입력 메시지 enum 관리

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,18 @@
 package view;
 
+import static view.InputMessage.*;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
 	public String enterReservationDate() {
-		System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+		System.out.println(INPUT_RESERVATION_DATE.getMessage());
 		return Console.readLine();
 	}
 
 	public String enterOrderMenus() {
-		System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+		System.out.println(INPUT_ORDER_MENUS.getMessage());
 		return Console.readLine();
 	}
 }
