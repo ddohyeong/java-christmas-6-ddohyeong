9ec9e2b8954d0ecee4cdaafe76e3d89472f6b7c4
ddohyeong <dohyeong7026@gmail.com>
2023-11-15 12:32:56 +0900

    feat(Application) : 크리스마스 이벤트 플래너 실행 기능 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import controller.ChristmasEventPlannerController;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		ChristmasEventPlannerController christmasEventPlannerController = new ChristmasEventPlannerController();
+		christmasEventPlannerController.run();
+	}
 }
